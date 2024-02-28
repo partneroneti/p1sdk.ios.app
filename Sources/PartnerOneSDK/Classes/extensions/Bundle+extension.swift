2ea845f695e48f816125818278df817b682c08e9
adrianomazucato <amazucato@gmail.com>
2024-02-28 18:10:35 -0300

    add prints

--- Sources/PartnerOneSDK/Classes/extensions/Bundle+extension.swift
+++ Sources/PartnerOneSDK/Classes/extensions/Bundle+extension.swift
@@ -3,6 +3,6 @@ import Foundation
 extension Bundle {
     class func getBundle() -> Bundle {
         print("Bundle \(Bundle(for: BaseViewController.self))")
-        return Bundle(for: BaseViewController.self)
+        return Bundle.module // Bundle(for: BaseViewController.self)
     }
 }
