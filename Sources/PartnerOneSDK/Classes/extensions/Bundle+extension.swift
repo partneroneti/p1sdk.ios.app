604d14a9b4e291deeaae538f23a3cb0110ca0ed1
adrianomazucato <amazucato@gmail.com>
2024-02-29 13:36:12 -0300

    mode strings

--- Sources/PartnerOneSDK/Classes/extensions/Bundle+extension.swift
+++ Sources/PartnerOneSDK/Classes/extensions/Bundle+extension.swift
@@ -2,9 +2,6 @@ import Foundation
 
 extension Bundle {
     class func getBundle() -> Bundle {
-        print("Bundle \(Bundle(for: BaseViewController.self))")
-        print("Bundle \(Bundle.allBundles)")
-        
         return Bundle.module // Bundle(for: BaseViewController.self)
     }
 }
